643a83a077937334398c10dd82f560a0bf46482c
unknown <amarjeet@baruzotech.com>
2023-01-20 16:17:35 +0530

    changed multer

--- helpers/multer.helpers.js
+++ helpers/multer.helpers.js
@@ -1,4 +1,5 @@
 import multer from 'multer';
+import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -7,8 +8,8 @@ const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, __dirname);
-    // db(null, path.join(__dirname, '/Images'));
+    // cb(null, __dirname);
+    cb(null, path.join(__dirname, '/uploads'));
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
